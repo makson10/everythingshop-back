3fdb5ab0a44f0f674abda1d74278bc8068547136
Glitch (glitch-hello-sqlite) <none>
2023-07-14 17:49:48 +0300

    feat: correct creating db and data inside it

--- sqlite.js
+++ sqlite.js
@@ -21,7 +21,7 @@ We're using the sqlite wrapper so that we can make async / await connections
 dbWrapper
     .open({
         filename: dbFile,
-        driver: sqlite3.Database
+        driver: sqlite3.Database,
     })
     .then(async dBase => {
         db = dBase;
@@ -45,19 +45,15 @@ dbWrapper
                 );
 
                 await db.run(
-                    "CREATE TABLE `feedback` (userName varchar(255) NOT NULL, date bigint(20) NOT NULL, feedbackText mediumtext NOT NULL, uniqueFeedbackId varchar(255) NOT NULL)"
-                );
-
-                await db.run(
-                    "INSERT INTO `feedback` (userName, date, feedbackText, uniqueFeedbackId) VALUES ('Макс m', 1685727861232, 'классный сайт братанчик, я б тебе отсосал', 'b18b121d-f167-47d6-81a1-2fd1e7e7a5d9')"
-                );
-
-                await db.run(
                     "CREATE TABLE `google_customers` (id varchar(255) NOT NULL, name varchar(255) NOT NULL, email varchar(255) NOT NULL, picture varchar(255) NOT NULL)"
                 );
 
                 await db.run(
                     "INSERT INTO `google_customers` (id, name, email, picture) VALUES ('104684886398385952212', 'Макс m', 'makson94021@gmail.com', 'https://lh3.googleusercontent.com/a/AGNmyxZTtA5onyRsnfgUXaqxIhygUmLl_ZOOPc8iR1sk=s96-c')"
+                );
+
+                await db.run(
+                    "CREATE TABLE `feedback` (author varchar(255) NOT NULL, date bigint(20) NOT NULL, feedbackText mediumtext NOT NULL, uniqueFeedbackId varchar(255) NOT NULL)"
                 );
 
                 await db.run(
@@ -67,8 +63,10 @@ dbWrapper
                 await db.run(
                     "INSERT INTO `products` (title, description, photo_id, creator, price, uniqueProductId, comments) VALUES ('Банан', 'Большой, вкусный, желтый банан', '5MjsX6QOWMLtTR2yaMbCVreNKCagMwUXoK1bNjg6.png', 'Pozitiv_ Maks', 100, '416419da-dfc6-4dd1-b1fc-637c2379d2cd', '[]'), ('засосались сучки', 'ммммммммммммм какой засос', 'Sn2lWCe0gEWpRhEck0A9rc6XheI1dkFlCjtguM7z.png', 'Макс m', 2000, 'a1481bb8-556b-452f-a1c8-2857eae5c1e0', '[]')"
                 );
-            } else {
-                console.log('Found db file');
+
+                await db.run(
+                    `INSERT INTO products (title, description, photoIds, creator, price, uniqueProductId, comments) VALUES ('83 или 73', 'хороший вопрос', '["13Fat6avVV9Mrj9Bsp_spx303MB6dTF_z","1AV1mlTG64vgtsjaZotqSxlUJCduz4EeR"]', 'Макс m', 7383, '01bfecce-7804-41e0-87ee-ab905df445f1', '[]')`
+                );
             }
         } catch (dbError) {
             console.error(dbError);
